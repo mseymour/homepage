1c91fb8bd35bab862503daa30a091744fc2e5da9
Mark Seymour <mark.seymour.ns@gmail.com>
2014-06-11 22:59:33 -0300

    Initial commit. The sass structure is based off of another project that I am working on, which was based off of this article by John Long: http://thesassway.com/beginner/how-to-structure-a-sass-project (I may just make everything into a single file later.) I use the latest version of the Sass (http://sass-lang.com), Susy (http://susy.odd-bird.net/), Breakpoint (http://breakpoint-sass.com/), and Bourbon (http://bourbon.io/) gems, and the HEAD version of Compass (http://compass-style.org/) on Ruby 2.1.2 as of this commit. I'll probably fix up some of the directory structure and such later, but for now this'll do. I will do up a README later.

---
+++ js/app.js
@@ -0,0 +1,60 @@
+$(document).ready(function( data ) {
+  
+  var autosearch = $('.autosearch');
+
+  $.getJSON('search.json', function( data ) {
+    var items = [];
+    $.each( data, function( key, val ) {
+      var selected = (key == 0) ? 'selected ' : '';
+      items.push( '<li class="' + selected + '" data-url="' + val['url'] + '" data-param="' + val['param'] + '">Search <b>' + val['name'] + '</b> for <b class="query"/></li>' );
+    });
+
+    $('.searchselectors').html(items.join( '' ));
+    setFormToSelected();
+  });
+  
+  $('.autosearch').on('keyup focus', function() {
+    if( autosearch.val() != '' ) {
+      $('.searchselectors.hidden').removeClass('hidden'); 
+    } else {
+      $('.searchselectors').addClass('hidden');
+    }
+    
+    $('.searchselectors li .query').text(autosearch.val());
+    
+    setFormToSelected();
+    
+  }).on('keydown', function( event ) {
+    if( !$('.searchselectors.hidden').hasClass('hidden') ) {
+      var selected = $('.searchselectors .selected');
+      var items = $('.searchselectors li');
+      
+      if ( event.keyCode == 38 || event.keyCode == 40 ) { // arrow up/down
+        var direction = event.keyCode == 38 ? 'prev' : 'next';
+        selected.toggleClass('selected');
+        
+        if( selected.is('li:last') && direction == 'next' ) {
+          items.first().toggleClass('selected');
+        } else if ( selected.is('li:first') && direction == 'prev' ) {
+          items.last().toggleClass('selected');
+        } else {
+          selected[direction]().toggleClass('selected');
+        }
+        
+        event.preventDefault();
+      }
+    
+      setFormToSelected();
+    }
+    
+  }).on('blur', function() {
+    $('.searchselectors').addClass('hidden');
+  });
+  
+});
+
+function setFormToSelected() {
+  var selected = $('.searchselectors .selected');
+  $('#search').attr('action', selected.data('url'));
+  $('.autosearch').attr('name', selected.data('param'));
+}
