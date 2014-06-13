9323e2037bf79ad86361a7a5601acc366412aaa0
Mark Seymour <mark.seymour.ns@gmail.com>
2014-06-13 00:20:47 -0300

    Refactoring search dropdown into a module. This will make it much easier to add things like live weather, maps, etc. a bit later on.

--- js/app.js
+++ js/app.js
@@ -1,60 +1,84 @@
-$(document).ready(function( data ) {
-  
-  var autosearch = $('.autosearch');
 
-  $.getJSON('search.json', function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-      var selected = (key == 0) ? 'selected ' : '';
-      items.push( '<li class="' + selected + '" data-url="' + val['url'] + '" data-param="' + val['param'] + '">Search <b>' + val['name'] + '</b> for <b class="query"/></li>' );
-    });
+// Search widget
 
-    $('.searchselectors').html(items.join( '' ));
-    setFormToSelected();
-  });
+var SearchWidget = (function () {
+  var s; // private alias to settings
+
+  return {
+    settings: {
+      searchForm: $("#search"),
+      searchInput: $(".autosearch"),
+      searchSelectors: $(".searchselectors"),
+      jsonURL: 'search.json'
+    },
+
+    init: function() {
+      s = this.settings;
+      this.bindUIActions();
+    },
+
+    bindUIActions: function() {
+      $.getJSON(s.jsonURL, function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          var selected = (key == 0) ? 'selected ' : '';
+          items.push( '<li class="' + selected + '" data-url="' + val['url'] + '" data-param="' + val['param'] + '">Search <b>' + val['name'] + '</b> for <b class="query"/></li>' );
+        });
+
+        s.searchSelectors.html(items.join( '' ));
+        SearchWidget.setFormToSelected();
+      });
   
-  $('.autosearch').focus().on('keyup focus', function() {
-    if( autosearch.val() != '' ) {
-      $('.searchselectors.hidden').removeClass('hidden'); 
-    } else {
-      $('.searchselectors').addClass('hidden');
-    }
-    
-    $('.searchselectors li .query').text(autosearch.val());
-    
-    setFormToSelected();
-    
-  }).on('keydown', function( event ) {
-    if( !$('.searchselectors.hidden').hasClass('hidden') ) {
-      var selected = $('.searchselectors .selected');
-      var items = $('.searchselectors li');
-      
-      if ( event.keyCode == 38 || event.keyCode == 40 ) { // arrow up/down
-        var direction = event.keyCode == 38 ? 'prev' : 'next';
-        selected.toggleClass('selected');
-        
-        if( selected.is('li:last') && direction == 'next' ) {
-          items.first().toggleClass('selected');
-        } else if ( selected.is('li:first') && direction == 'prev' ) {
-          items.last().toggleClass('selected');
+      s.searchInput.focus().on('keyup focus', function() {
+        if( s.searchInput.val() != '' ) {
+          $('.searchselectors.hidden').removeClass('hidden'); 
         } else {
-          selected[direction]().toggleClass('selected');
+          s.searchSelectors.addClass('hidden');
         }
+    
+        $('.searchselectors li .query').text(s.searchInput.val());
+    
+        SearchWidget.setFormToSelected();
+    
+      }).on('keydown', function( event ) {
+        if( !$('.searchselectors.hidden').hasClass('hidden') ) {
+          var selected = $('.searchselectors .selected');
+          var items = $('.searchselectors li');
+      
+          if ( event.keyCode == 38 || event.keyCode == 40 ) { // arrow up/down
+            var direction = event.keyCode == 38 ? 'prev' : 'next';
+            selected.toggleClass('selected');
         
-        event.preventDefault();
-      }
+            if( selected.is('li:last') && direction == 'next' ) {
+              items.first().toggleClass('selected');
+            } else if ( selected.is('li:first') && direction == 'prev' ) {
+              items.last().toggleClass('selected');
+            } else {
+              selected[direction]().toggleClass('selected');
+            }
+        
+            event.preventDefault();
+          }
     
-      setFormToSelected();
-    }
+          SearchWidget.setFormToSelected();
+        }
     
-  }).on('blur', function() {
-    $('.searchselectors').addClass('hidden');
-  });
-  
-});
+      }).on('blur', function() {
+        s.searchSelectors.addClass('hidden');
+      });
+    },
 
-function setFormToSelected() {
-  var selected = $('.searchselectors .selected');
-  $('#search').attr('action', selected.data('url'));
-  $('.autosearch').attr('name', selected.data('param'));
-}
+    setFormToSelected: function() {
+      var selected = $('.searchselectors .selected');
+      s.searchForm.attr('action', selected.data('url'));
+      s.searchInput.attr('name', selected.data('param'));
+    }
+
+  };
+})();
+
+// Initialize widgets, etc.
+
+(function () {
+  SearchWidget.init();
+})();
