2b5fc44f5097bf69f23f78d9e45284fe599f2f5c
Mark Seymour <mark.seymour.ns@gmail.com>
2014-06-17 16:50:19 -0300

    Rewrote keyboard navigation and DOM rendering to allow for groups. (Adapted from http://stackoverflow.com/a/24252296/780109)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
-
 // Search widget
 
 var SearchWidget = (function () {
   var s; // private alias to settings
+  
+  function populateSearchSelector(data) {
+    var el = [];
+    $.each( data, function( grouptitle, sites ) {
+      // Groups
+      var group = $( '<li class="group"><span class="title" /><ul class="sites" /></li>' );
+      $('.title', group).text(grouptitle);
+      var sitelist = [];
+      $.each ( sites, function( id, siteitem ) {
+        var site = $( '<li class="item"><span>Search <b class="title" /> for <b class="query" /></span></li>' );
+        site.data('url', siteitem['url']);
+        $('.title', site).text(siteitem['name']);
+        $('.query', site).text(s.searchInput.val());
+        sitelist.push(site);
+      });
+      $('.sites', group).append(sitelist);
+      el.push(group);
+    });
+    s.searchSelectors.append(el);
+    $('.item:first', s.searchSelectors).addClass('selected');
+    console.log($('.item:first', s.searchSelectors));
+  }
 
   return {
     settings: {
@@ -15,20 +36,22 @@ var SearchWidget = (function () {
 
     init: function() {
       s = this.settings;
+      
+      // Populate the "dropdown" with our items from the file set by s.jsonURL (./search.json by default)
+      $.getJSON(s.jsonURL, function( data ) {
+        populateSearchSelector( data );
+      });
+      
       this.bindUIActions();
     },
 
     bindUIActions: function() {
-      // Populate the "dropdown" with our items from the file set by s.jsonURL (./search.json by default)
-      $.getJSON(s.jsonURL, function( data ) {
-        var items = [];
-        $.each( data, function( key, val ) {
-          var selected = (key == 0) ? 'selected ' : '';
-          items.push( '<li class="' + selected + '" data-url="' + val['url'] + '"><span>Search <b>' + val['name'] + '</b> for <b class="query"/></span></li>' );
-        });
-
-        s.searchSelectors.html(items.join( '' ));
-      });
+      var $menu = s.searchSelectors,
+          $items,
+          $selectedItem,
+          selectedIndex = 0;
+          
+      console.log($menu, $items, $selectedItem, selectedIndex);
       
       s.searchForm.on('submit', function ( event ) {
         event.preventDefault();
@@ -44,44 +67,32 @@ var SearchWidget = (function () {
         }
         
         // Set the (by default empty) b.query element for each selection to our query from the textbox.
-        s.searchSelectors.find('li .query').text(s.searchInput.val());
+        s.searchSelectors.find('.item .query').text(s.searchInput.val());
       
       }).on('keydown', function( event ) {
-        if( !s.searchSelectors.filter('.hidden').hasClass('hidden') ) {
-          var selected = s.searchSelectors.find('.selected');
-          var items = s.searchSelectors.find('li');
-      
-          if ( event.keyCode == 38 || event.keyCode == 40 ) {
-            // setting selection state on previous/next sibling on pressing arrow up/down
-            var direction = event.keyCode == 38 ? 'prev' : 'next';
-            selected.toggleClass('selected');
-        
-            if( selected.is('li:last') && direction == 'next' ) {
-              items.first().toggleClass('selected');
-            } else if ( selected.is('li:first') && direction == 'prev' ) {
-              items.last().toggleClass('selected');
-            } else {
-              selected[direction]().toggleClass('selected');
-            }
-        
-            event.preventDefault();
-          }
+        $items = $menu.find(".item");
+        $selectedItem = $menu.find(".selected");
+        switch(event.keyCode) {
+            case 40:  // down arrow
+                $selectedItem.removeClass("selected");
+                selectedIndex = (selectedIndex + 1) % $items.length;
+                $selectedItem = $items.eq(selectedIndex).addClass("selected");
+                break;
+            case 38:  // up arrow
+                $selectedItem.removeClass("selected");
+                selectedIndex = (selectedIndex - 1) % $items.length;
+                $selectedItem = $items.eq(selectedIndex).addClass("selected");
+                break;
         }
-    
       }).on('blur', function() {
         setTimeout(function() {
           s.searchSelectors.addClass('hidden');
         }, s.blurDelay);
       });
       
-      s.searchSelectors.on('click', 'li span', function( event ) {
-        // On clicking, we switch the selection to the clicked item,
-        // and then run the startSearch function.
-        var parent = $(this).parent();
-        if ( !parent.hasClass('.selected') ) {
-          parent.siblings('.selected').removeClass('selected');
-          parent.addClass('selected');
-        }
+      s.searchSelectors.on('click', '.item span', function( event ) {
+        s.searchSelectors.find('.selected').removeClass('selected');
+        $(this).parent().addClass('selected');
         SearchWidget.startSearch();
       });
       
@@ -89,7 +100,7 @@ var SearchWidget = (function () {
 
     startSearch: function() {
       // Get selected item, and navigate.
-      var selected = s.searchSelectors.find('.selected');
+      var selected = $('.selected', s.searchSelectors);
       window.location = selected.data('url').replace('%s',s.searchInput.val());
     }
 
